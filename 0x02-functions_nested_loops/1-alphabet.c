eaa92e55b93df0d235ac435b020835772392a029
dodow <dodow0016@gmail.com>
2022-12-13 18:17:57 +0000

    doooooooooo

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,12 @@
  * Description: It prints the alphabet in lowercase fallowed by a new line
  * Return: Nothing.
  */
-void print_alphabet()
+void print_alphabet(void)
 {
-char ch;
-for (ch = 'a' ;ch <='z'; ch++ )
+char ch ;
+for (ch = 'a' ; ch <= 'z'; ch++ )
 {
 putchar(ch);
 }
-putchar('\n');
+putchar( '\n' ) ;
 }
