bf0400958c43810d8c082bf830d9b418176fa136
dodow <dodow0016@gmail.com>
2022-12-13 19:28:25 +0000

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower- Check description
+ * Description: It check lowercase fallowed by a new line
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+if (c >='a' && c<='z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
