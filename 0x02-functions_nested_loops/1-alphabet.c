01d72c0e42ad6728602759891b2190562953df38
dodow <dodow0016@gmail.com>
2022-12-13 17:56:11 +0000

    1-alphabet.c goo

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+/**  main - Entry point
+* return: Always 0 .
+*/
+void print_alphabet();
+int main (void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet()
+{
+char ch;
+for(ch ='a' ;ch <='z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+}
