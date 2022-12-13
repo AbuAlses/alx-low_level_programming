3bb76079dd62ba437d0ddd619eed2335d1600ede
dodow <dodow0016@gmail.com>
2022-12-13 20:08:53 +0000

    thing

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,19 @@
 /**
- * _isalpha - Check alpha
+ * _isalpha - Check Holberton
  * @c: An input character
  * Description: function returns 1 if the character is a
  * letter, lowercase or uppercase.
  * Return: 1 or 0 in otherwise.
  */
-
 int _isalpha(int c)
 {
-if ((c >= 'a' && c >= 'A') ||  (c <= 'z' && c <= 'Z')) || (c >= 0 && c <= 9))
-{
-return (1);
+char lower, upper;
+int isletter = 0;
+for (lower = 'a'; lower <= 'z'; lower++)
+{for (upper = 'A'; upper <= 'Z'; upper++)
+{if (c == lower || c == upper)
+isletter = 1;
 }
-else
-{
-return (0);
-}}
+}
+return (isletter);
+}
