198383850a5f06388872364e406d314126cd2bc8
dodow <dodow0016@gmail.com>
2022-12-14 16:01:52 +0000

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,22 @@
 #include "main.h"
-#include<stdio.h>
-
 /**
- * jack_bauer - Check Holberton
+ * jack_bauer - Check main.h
  * Description: function prints every minute in one day
  * Return: Nothing
  */
 void jack_bauer(void)
 {
 int H, M;
-for (H = 0; H < 24; H++)\{
+for (H = 0; H < 24; H++)
+{
 for (M = 0; M < 60; M++)
 {
-putchar((H / 10) + '0');
-putchar((H % 10) + '0');
-putchar(':');
-putchar((M / 10) + '0');
-putchar((M % 10) + '0');
-putchar('\n');
+_putchar((H / 10) + '0');
+_putchar((H % 10) + '0');
+_putchar(':');
+_putchar((M / 10) + '0');
+_putchar((M % 10) + '0');
+_putchar('\n');
 }
 }
 }
