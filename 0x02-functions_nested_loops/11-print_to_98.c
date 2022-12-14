3f88eb098d5a79ff1d9f477781b07e1a0e2ebeae
dodow <dodow0016@gmail.com>
2022-12-14 16:28:18 +0000

    this to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98print_to_98 check code
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+for(int x = n; x <= 98 ;x++)
+{
+_putchar(x);
+_putchar(' ');
+}
+}
