a20e61c68e0c48bebfd4bc6a64acad21bdaa2256
dodow <dodow0016@gmail.com>
2022-12-19 20:35:17 +0000

    dodo

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,14 @@
  */
 void rev_string(char *s)
 {
-	int len = 0, i = 0;
-	char aux;
-
-	while (s[len] != '\0')
-		len++;
-
-	
+int len = 0, i = 0;
+char aux;
+while (s[len] != '\0')
+		len++;	
 while (i < len--)
-	{
-		aux = s[i];
-		s[i++] = s[len];
-		s[len] = aux;
-	}
+{
+	aux = s[i];
+s[i++] = s[len];
+s[len] = aux;
+}
 }
