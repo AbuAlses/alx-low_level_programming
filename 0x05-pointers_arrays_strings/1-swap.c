b0e0bf3801802c48a4786ec39d25777b4aa6a26a
dodow <dodow0016@gmail.com>
2022-12-19 19:58:39 +0000

    pointer1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,16 @@
 #include "main.h"
+/**
+ * swap_int switch to value by referance
+ * @x int 
+ * @*b point to int
+ * @*b point to int
+ * return 0
+ */
 void swap_int(int *a, int *b)
 {
 int x;
 x = *a;
 *a = *b;
-*b= x;
+*b = x;
 
 }
